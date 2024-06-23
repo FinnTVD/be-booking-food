171498157aede944464b46fbcc5a26c0f1c188b9
FinnTVD <finnit.th@gmail.com>
2024-06-23 20:14:18 +0700

    update

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = () => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: "https://be-booking-food.vercel.app/",
+      apiToken: "6ws243F4tf0LwHmLze62zwHy",
+      appFilter: "be-booking-food",
+      teamFilter: "finnitthgmailcoms-projects",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
